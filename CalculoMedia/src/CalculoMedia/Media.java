86076bbbf0ca8e6c4e7cc0f0c2e65935a2c2bf93
glluna <gabrielluna98@hotmail.com>
2024-01-29 15:07:41 -0300

    Tarefa- Cálculo média

---
+++ CalculoMedia/src/CalculoMedia/Media.java
@@ -0,0 +1,23 @@
+package CalculoMedia;
+
+public class Media {
+
+	public static void main(String[] args) {
+		double nota1 = 8.0;
+		
+		double nota2 = 6.0;
+		
+		double nota3 = 4.0;
+		
+		double nota4 = 7.0;
+		
+        double media = (nota1 + nota2 + nota3 + nota4)/4;
+        System.out.println(media);
+    	
+	}
+	
+	
+	
+	
+
+}
