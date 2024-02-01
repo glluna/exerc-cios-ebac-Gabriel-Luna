c7ec8e67e591b5a097436ebc94b017bd9e01fd75
glluna <gabrielluna98@hotmail.com>
2024-02-01 16:29:38 -0300

    Tarefa_Wrapper

---
+++ README.md
@@ -0,0 +1,1 @@
+# exerc-cios-ebac-Gabriel-Luna
---
+++ Tarefa_wrapper/src/Br/Luna/Wrapper.java
@@ -0,0 +1,15 @@
+package Br.Luna;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		int a = 12;
+		
+		Integer b = a;
+		
+		System.out.println("Valor primitivo: "+ a);
+		System.out.println("Valor Wrapper: "+ b);
+
+	}
+
+}
